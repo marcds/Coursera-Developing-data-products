77b19aaf15bae6282ee23ebd9470ca2e6f05e8ee
marcds <mdesolis1@gmail.com>
2015-05-24 11:14:32 -0700

    Updated city filters in server.R, project descriptioni in ui.R and aligned description code.

--- global.R
+++ global.R
@@ -1,7 +1,7 @@
 # global.r
 
 library(shiny)
-require(rCharts)
+library(rCharts)
 library(ggplot2)
 library(RCurl)
 library(jsonlite)
@@ -14,10 +14,10 @@ food_url <- getURLContent("http://api.fda.gov/food/enforcement.json?search=repor
 # Parse the url in json data format
 food_js_url <- fromJSON(food_url, simplifyVector=TRUE)
 
-# Extract data contained in "results" in a data frame
+# Extract results in data frame
 tmp_f_df <-food_js_url[["results"]]
 
-# Remove the columns not required
+# Select only columns required
 food_df <- select(tmp_f_df, -contains("@"), -reason_for_recall, -code_info, -openfda, -initial_firm_notification, -distribution_pattern, -country)
 
 # Parse the date vaiables
@@ -26,3 +26,4 @@ food_df$report_date <- ymd(food_df$report_date)
 
 # Change classification variable to ordered factor
 food_df$classification <- factor(food_df$classification, levels = c("Class I", "Class II", "Class III"))
+# food_df$classification <- as.ordered(factor(food_df$classification, levels = c("Class I", "Class II", "Class III")))
